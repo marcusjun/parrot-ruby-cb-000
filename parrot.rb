50b0295a60ce8f9083ed3cc3c761ba730590977e
Marcus Jun <mjun@calstate.edu>
2018-04-04 17:43:37 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+def parrot
+
 def parrot(string)
+  if (string===null)
+    return "Squak!"
+  end
   puts ${string}
   string
 end
